db29fa5b32c565f2cc95f950d73643e16412600b
DouniaVaillant <dounia.vaillant@gmail.com>
2022-11-09 13:08:26 +0100

    inscription finalisée avec succès (date d'inscription automatique incluse)

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -3,47 +3,45 @@
 class AppController
 {
 
-    public static function index()
-    {
-        include(VIEWS . 'app/index.php');
+  public static function index()
+  {
+    include(VIEWS . 'app/index.php');
+  }
+
+  // public static function createBook()
+  // {
+
+
+
+
+  //  include(VIEWS."app/createBook.php" ) ;
+  // }
+
+  // ! inscription il ne manque que cette partie pour pouvoir ajouter des users
+
+  public static function registration()
+  {
+
+    if (!empty($_POST)) {
+
+      User::create([
+        'roles' => 'ROLE_USER',
+        'lastname' => $_POST['lastname'],
+        'firstname' => $_POST['firstname'],
+        'pseudo' => $_POST['pseudo'],
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+        'birthday' => $_POST['birthday'],
+        'way' => $_POST['way'],
+        'address' => $_POST['address'],
+        'city' => $_POST['city'],
+        'postal_code' => $_POST['postal_code'],
+        'country' => $_POST['country'],
+        'gender' => $_POST['gender']
+      ]);
     }
 
-    // public static function createBook()
-    // {
-     
-      
-     
-     
-    //  include(VIEWS."app/createBook.php" ) ;
-    // }
 
-    // ! inscription il ne manque que cette partie pour pouvoir ajouter des users
-
-    public static function registration()
-    {
-     
-      if (!empty($_POST))
-      {
-        User::create([
-            'roles' => 'ROLE_USER',
-            'lastname' => $_POST['lastname'],
-            'firstname' => $_POST['firstname'],
-            'pseudo' => $_POST['pseudo'],
-            'email' => $_POST['email'],
-            'password' => $_POST['password'],
-            'birthday' => $_POST['birthday'],
-            'way' => $_POST['way'],
-            'address' => $_POST['address'],
-            'city' => $_POST['city'],
-            'postal_code' => $_POST['postal_code'],
-            'country' => $_POST['country'],
-            'gender' => $_POST['gender'],
-            'date_registration' => new DateTime()
-        ]);
-      }
-     
-     
-     include(VIEWS."app/registration.php" ) ;
-    }
-
+    include(VIEWS . "app/registration.php");
+  }
 }
--- src/models/User.php
+++ src/models/User.php
@@ -6,7 +6,7 @@ class User extends Db
     public static function create(array $data)
     {
      
-      $request = "INSERT INTO user (roles, lastname, firstname, pseudo, email, password, birthday, way, address, city, postal_code, country, gender, date_registration) VALUES (:roles, :lastname, :firstname, :pseudo, :email, :password, :birthday, :way, :address, :city, :postal_code, :country, :gender, :date_registration)";
+      $request = "INSERT INTO user (roles, lastname, firstname, pseudo, email, password, birthday, way, address, city, postal_code, country, gender, date_registration) VALUES (:roles, :lastname, :firstname, :pseudo, :email, :password, :birthday, :way, :address, :city, :postal_code, :country, :gender, NOW())";
       $response = self::getDb()->prepare($request);
       $response->execute(self::htmlspecialchars($data));
 
