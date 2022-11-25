8e55bcc4e1511ed032d322e2aba93d2dcfd0af40
DouniaVaillant <dounia.vaillant@gmail.com>
2022-11-25 03:18:00 +0100

    Style de showBook

--- views/app/book/showBook.php
+++ views/app/book/showBook.php
@@ -1,29 +1,51 @@
 <?php include(VIEWS . '_partials/header.php'); ?>
 <?php $user = User::findById(['id' => $book['id_user']]); ?>
 
+<div class="page">
+    <div class="leftShowBook"></div>
+    <div class="rightShowBook">
+        <div class="allInfos">
+            <div class="leftInfos">
+                <div class="targetStatus">
+                    <div class="targetShowBook"><?= $book['target_reader']; ?></div>
+                    <div class="statusShowBook"><?= $book['status']; ?></div>
+                </div>
+                <div class="synopsisInfos">
+                    <div class="caseSynopsis">
+                        <p><?= $book['synopsis']; ?></p>
+                    </div>
+                    <div class="infos">
+                        <p><strong>Auteur: </strong><?= $book['author']; ?></p>
+                        <p><strong>Editeur: </strong><?= $book['editor']; ?></p>
+                        <p><strong>Publié en: </strong><?= $book['date_publication']; ?></p>
+                    </div>
+                </div>
+                <div class="userComment">
+                    <a class="userShowBook" href="<?= BASE_PATH . 'user/profile?id=' . $book['id_user']; ?>">
+                        <img src="<?= BASE . 'upload/photos/profile/' . $user['photo_profile']; ?>" style="height: 10vh; overflow: hidden;  border-radius:150px;" alt="Photo de profil">
+                        <p>
+                            <?= $user['pseudo']; ?>
+                        </p>
+                    </a>
+                    <div class="caseUserComment">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus omnis quam est voluptas animi architecto expedita hic facere sed possimus necessitatibus odit iste corporis labore magnam molestias, excepturi perspiciatis odio?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="rightInfos">
+                <h2><?= $book['title']; ?></h2>
+                <img src="<?= BASE . 'upload/book/' . $book['photo']; ?>" alt="Couverture de <?= $book['title']; ?>" height="400">
+            </div>
 
-<div class="btn text-light" style="background: grey;"><?= $book['target_reader']; ?></div>
-<div class="btn bg-lightBrown"><?= $book['status']; ?></div>
-<h2><?= $book['title']; ?></h2>
-<img src="<?= BASE . 'upload/book/' . $book['photo']; ?>" alt="Couverture de <?= $book['title']; ?>" height="400">
 
-<p><strong>Synopsis: <br></strong><?= $book['synopsis']; ?></p>
-<p><strong>Auteur: </strong><?= $book['author']; ?></p>
-<p><strong>Editeur: </strong><?= $book['editor']; ?></p>
-<p><strong>Publié en: </strong><?= $book['date_publication']; ?></p>
+        </div>
+
+    </div>
 
 
-<div class="" style="overflow: hidden; width: 10vh; border-radius:150px; background-position: center/cover;">
-    <a href="<?= BASE_PATH . 'user/profile?id=' . $book['id_user']; ?>">
-        <img src="<?= BASE . 'upload/photos/profile/' . $user['photo_profile']; ?>" style="height: 10vh; overflow: hidden;  border-radius:150px;" alt="Photo de profil">
-    </a>
+
+
 </div>
-<?= $user['pseudo']; ?>
-
-
-
-
-
 
 
 
